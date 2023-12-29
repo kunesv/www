5bd5b0f904d0bd64f06553168c8ce7ae0e921399
Vaclav Kunes <kunesv@users.noreply.github.com>
2023-12-29 20:53:15 +0100

    Add files via upload

--- imageLoader.js
+++ imageLoader.js
@@ -6,41 +6,80 @@ document.getElementById('fileInput').addEventListener('change', function (e) {
     img.src = URL.createObjectURL(e.target.files[0]);
 
     img.onload = function () {
-        var maxWidth = window.innerWidth;
-        var maxHeight = window.innerHeight;
+        document.getElementById('loadImage').classList.add('hidden')
 
-        var ratio = 0;
+        var ratio = 1;
         var width = img.width;
         var height = img.height;
 
-        // Calculate ratio for height and width
+        var maxWidth = window.innerWidth
+        var maxHeight = window.innerHeight
+
         if (width > height) {
             ratio = maxWidth / width;
         } else {
             ratio = maxHeight / height;
         }
 
-        // New dimensions for image
         width = width * ratio;
         height = height * ratio;
 
-        // Set new canvas dimensions
         canvas.width = width;
         canvas.height = height;
 
-        ctx.drawImage(img, 0, 0, width, height);
-        document.getElementById('fileInput').classList.add('hidden')
+
+        ctx.translate(width/2, height/2);
+        ctx.drawImage(img, -width/2, -height/2, width, height);
+
+        var rotationCount = 0;
+        
+        canvas.addEventListener('click', function (e) {
+            rotationCount++;
+
+            ratio = 1;
+            width = img.width;
+            height = img.height;
+
+            maxWidth = window.innerWidth
+            maxHeight = window.innerHeight
+            
+
+            if (rotationCount%2 === 1) {
+                if (width > height) {
+                    ratio = maxWidth / height;
+                } else {
+                    ratio = maxHeight / width;
+                }
+
+                width = width * ratio;
+                height = height * ratio;
+
+                canvas.width = height 
+                canvas.height = width
+                
+                ctx.translate(height/2, width/2);
+            } else {
+                if (width > height) {
+                    ratio = maxWidth / width;                    
+                } else {
+                    ratio = maxHeight / height;
+                }
+
+                width = width * ratio;
+                height = height * ratio;
+
+                canvas.width = width 
+                canvas.height = height
+                
+                ctx.translate(width/2, height/2);
+            }
+
+            ctx.rotate(Math.PI/2 * rotationCount);
+            ctx.drawImage(img, -width/2, -height/2, width, height);
+        });
     };
 });
 
-// var canvas = document.getElementById('myCanvas');
-// var ctx = canvas.getContext('2d');
-
-// ctx.fillStyle = 'red';
-// ctx.fillRect(20, 20, 100, 100);
-
-// var img = new Image();
-// img.src = 'path-to-image';
-// img.onload = function() {
-//     ctx.drawImage(img, 0, 0, 300, 150);
-// };
+document.getElementById('loadImage').addEventListener('click', function (e) {
+    document.getElementById('fileInput').click();
+});
